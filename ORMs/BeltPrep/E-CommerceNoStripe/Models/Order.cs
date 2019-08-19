55739f9d1ad2b7e44586f6b6d685fafbbf473aca
M <hokushrine@gmail.com>
2019-08-19 16:47:12 -0700

    commit

--- ORMs/BeltPrep/E-CommerceNoStripe/Models/Order.cs
+++ ORMs/BeltPrep/E-CommerceNoStripe/Models/Order.cs
@@ -7,6 +7,8 @@ namespace E_CommerceNoStripe.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Quantity { get; set; }
         public int CustomerId { get; set; }
         public int ProductId { get; set; }
